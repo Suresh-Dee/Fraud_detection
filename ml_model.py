7325ac9671b488104657178255099beabbd05e9a
Suresh_Dee <surshu01@gmail.com>
2022-07-23 13:36:02 +0530

    print line added

--- ml_model.py
+++ ml_model.py
@@ -1,8 +1,2 @@
-print("adding all data processing code related to fraud detection")
-a=5
-b=3
-def sum(a,b):
-    c=a+b
-    return c
-print(sum(25,5))
-
+print("new model added and try to solve conflic")
+print("new line")
